740852b91e70df9642188a881ad5b29813b3e351
Siyabonga <siya3.cyril@gmail.com>
2023-02-08 22:01:27 +0200

    Added functions for simple arithmetic,string length and accessing list item

--- index.dart
+++ index.dart
@@ -1,1 +1,36 @@
-void main() {}
+void main() {
+  int numOne = 25, numTwo = 5;
+  String myName = "Siyabonga Ntshingila";
+  List capitalCities = ["Nairobi", "Johannesburg", "Kairo"];
+
+  print(addTwo(numOne, numTwo));
+  print(subtractTwo(numOne, numTwo));
+  print(multiplyTwo(numOne, numTwo));
+  print(divideTwo(numOne, numTwo));
+  print(stringLength(myName));
+  print(getFirstElement(capitalCities));
+}
+
+int addTwo(int x, int y) => x + y;
+
+int subtractTwo(int x, int y) {
+  if (x >= y) {
+    return x - y;
+  } else {
+    return y - x;
+  }
+}
+
+int multiplyTwo(int x, int y) => x * y;
+
+double divideTwo(int x, int y) {
+  if (x >= y) {
+    return x / y;
+  } else {
+    return y / x;
+  }
+}
+
+int stringLength(String word) => word.length;
+
+String getFirstElement(List container) => container[0];
